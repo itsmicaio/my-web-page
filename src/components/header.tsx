fe605e222a45560a6ad4d6af909099d60df69d85
Caio Fuzatto <caiofuzattom@gmail.com>
2023-08-27 19:29:14 -0300

    add avatar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
     <header className="bg-green w-full flex flex-col items-center mb-7">
-      <a className="block" href="/">
+      <a className="flex justify-center items-center gap-3" href="/">
+        <img
+          className="w-14 h-14 rounded-full"
+          src="/uploads/avatar.jpeg"
+          alt="Avatar Caio Fuzatto"
+        />
         <h1 className="my-8 max-w-xs text-white text-2xl">Caio Fuzatto</h1>
       </a>
     </header>
