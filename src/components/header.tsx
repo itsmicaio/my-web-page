159a02bad428ae3b70c73f723f43fb2493d09d1a
Caio Fuzatto <caiofuzattom@gmail.com>
2023-08-27 19:49:15 -0300

    header position fixed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,8 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="bg-green w-full flex flex-col items-center mb-7">
+    <>
+    <header className="bg-green w-full flex flex-col items-center fixed top-0 left-0">
       <a className="flex justify-center items-center gap-3" href="/">
         <img
           className="w-14 h-14 rounded-full"
@@ -12,6 +13,8 @@ const Header = () => {
         <h1 className="my-8 max-w-xs text-white text-2xl">Caio Fuzatto</h1>
       </a>
     </header>
+    <div className="h-28"></div>
+    </>
   );
 };
 
