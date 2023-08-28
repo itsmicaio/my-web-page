97bd48a059ee59c810237429e323721e855b24c6
Caio Fuzatto <caiofuzattom@gmail.com>
2023-08-28 10:27:07 -0300

    improve avatar and header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,11 +3,11 @@ import React from "react";
 const Header = () => {
   return (
     <>
-    <header className="bg-green w-full flex flex-col items-center fixed top-0 left-0">
+    <header className="bg-green w-full flex flex-col items-center fixed top-0 left-0 shadow-md">
       <a className="flex justify-center items-center gap-3" href="/">
         <img
-          className="w-14 h-14 rounded-full"
-          src="/uploads/avatar.jpeg"
+          className="w-14 h-14 rounded-full border-solid border-white border-[1px]"
+          src="/uploads/avatar.png"
           alt="Avatar Caio Fuzatto"
         />
         <h1 className="my-8 max-w-xs text-white text-2xl">Caio Fuzatto</h1>
